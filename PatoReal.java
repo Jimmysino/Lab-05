115fff47bd5e58d0df5de2c329f7ba6b71adf938
Jimmy Melendez <leitotapia07@gmail.com>
2026-05-09 17:39:52 -0500

    feat: Clases implementadas añadidadas

---
+++ PatoReal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package patoadaptador;
+
+/**
+ *
+ * @author sistemas
+ */
+public class PatoReal implements Pato{
+
+    @Override
+    public void cuaquear() {
+        System.out.println("Cuac Cuac");
+    }
+
+    @Override
+    public void volar() {
+        System.out.println("Estoy volando");
+    }
+    
+}
